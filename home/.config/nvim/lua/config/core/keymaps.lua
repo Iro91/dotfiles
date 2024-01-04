56fce72a7e0a857b5b5515575348b8539ec7605d
Ivan <ivromero19@gmail.com>
2024-01-03 21:54:06 -0800

    Happy with nvim

--- home/.config/nvim/lua/config/core/keymaps.lua
+++ home/.config/nvim/lua/config/core/keymaps.lua
@@ -1,38 +1,40 @@
 -- Set the leader key to space
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Normal Mode Mappings
-vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
 -- Insert Mode Mappings
-vim.keymap.set('i', 'jk', '<escape>')
-vim.keymap.set('i', '<C-v>', '<ESC>"+pa')
+keymap.set('i', 'jk', '<escape>')
+keymap.set('i', '<C-v>', '<ESC>"+pa')
 
 -- Visual Mode Mappings
-vim.keymap.set('n', '<leader>y', '"+Y')
-vim.keymap.set('v', '<C-c>', '"+Y')
-vim.keymap.set('v', '<leader>y', '"+y')
+keymap.set('n', '<leader>y', '"+Y')
+keymap.set('v', '<C-c>', '"+Y')
+keymap.set('v', '<leader>y', '"+y')
 
 -- Save application with good old <C-S>
-vim.keymap.set('n', '<C-S>', ':update<CR>', {silent = true})
-vim.keymap.set('v', '<C-S>', '<C-C>:update<CR>', {silent = true})
-vim.keymap.set('i', '<C-S>', '<C-O> <ESC>:update<CR>', {silent = true})
+keymap.set('n', '<C-S>', ':update<CR>', {silent = true})
+keymap.set('v', '<C-S>', '<C-C>:update<CR>', {silent = true})
+keymap.set('i', '<C-S>', '<C-O> <ESC>:update<CR>', {silent = true})
 
 -- Jump to beginning of line
-vim.keymap.set('n', 'H', '^')
-vim.keymap.set('v', 'H', '^')
+keymap.set('n', 'H', '^')
+keymap.set('v', 'H', '^')
 
 -- Jump to end of line
-vim.keymap.set('n', 'L', '$')
-vim.keymap.set('v', 'L', '$')
+keymap.set('n', 'L', '$')
+keymap.set('v', 'L', '$')
 
-vim.keymap.set('n', '<leader><leader>', "<cmd>so<CR>")
-vim.keymap.set('n', '<leader>c', "<cmd>nohlsearch<CR>")
+keymap.set('n', '<leader><leader>', "<cmd>so<CR>")
+keymap.set('n', '<leader>c', "<cmd>nohlsearch<CR>")
 
 -- Buffer maangement
-vim.keymap.set('n', '<leader>h', ':bprev<CR>')
-vim.keymap.set('n', '<leader>o', '<C-^>')
-vim.keymap.set('n', '<leader>l', ':bnext<CR>')
+keymap.set('n', '<leader>h', ':bprev<CR>')
+keymap.set('n', '<leader>o', '<C-^>')
+keymap.set('n', '<leader>l', ':bnext<CR>')
 
