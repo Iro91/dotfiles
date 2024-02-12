68da1993b512e90dcdd64b24b6c44e9357a47a3c
Ivan Romero <iromer11@its.jnj.com>
2024-02-12 11:42:42 -0800

    Enable git blame

--- home/.config/nvim/lua/plugins/gitsigns.lua
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -3,7 +3,14 @@ return {
         "lewis6991/gitsigns.nvim",
         event = "VeryLazy",
         config = function()
-            require("gitsigns").setup()
+            local nnoremap = require("user.keymap_utils").nnoremap
+            local gitsigns = require("gitsigns")
+            gitsigns.setup()
+            require("user.keymap_utils").nnoremap('<leader>gb',  function()
+                gitsigns.toggle_current_line_blame()
+                end,
+                {desc = "Toggle Git Blame" }
+            )
         end,
     },
 }
