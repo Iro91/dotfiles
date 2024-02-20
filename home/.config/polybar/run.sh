1eb5162aefe0838febc3d3475862672bdbd87d97
Ivan Romero <ivromero19@gmail.com>
2024-02-20 07:17:50 -0800

    Added beginning of polybar

--- home/.config/polybar/run.sh
+++ home/.config/polybar/run.sh
@@ -20,38 +20,7 @@ EOF
 #-------------------------------------------------------------------------------
 function Main() {
     [[ $VERBOSE == "true" ]] && set -x
-    Paint ./cuts/preview.sh
-    return 0
-
-    #QuietRun ./blocks/preview.sh
-    #QuietRun ./colorblocks/preview.sh
-    QuietRun ./cuts/preview.sh
-    QuietRun ./docky/preview.sh
-    QuietRun ./forest/preview.sh
-    #QuietRun ./grayblocks/preview.sh
-    QuietRun ./hack/preview.sh
-    QuietRun ./material/preview.sh
-    #QuietRun ./shades/preview.sh
-    #QuietRun ./shapes/preview.sh
-}
-
-function QuietRun() {
-    echo "Watching $1"
-    "$1" &> /dev/null
-    #Paint "$1"
-    read -r
-    killall polybar
-}
-
-function Paint() {
-    local wp=/home/iro/.config/wallpaper/aurora.jpg
-    local cm="$(dirname "$1")/scripts/pywal.sh"
-    if [ -e "$cm" ]; then
-        echo "$cm" "$wp"
-        "$cm" "$wp" || echo "Failed to paint: $cm"
-        sleep 1
-    fi
-
+    polybar -r -c ./config.ini bar
 }
 
 #-------------------------------------------------------------------------------
